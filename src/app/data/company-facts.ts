7ab1e1801bc7e193d9fe2dc5f7cc2fc66cace26f
IsharaXD <ishara5678bhagya@gmail.com>
2026-08-14 11:27:34 +0530

    5th commit

--- src/app/data/company-facts.ts
+++ src/app/data/company-facts.ts
@@ -1,6 +1,6 @@
 export const COMPANY_STATS = [
   { value: '1974', label: 'Group Established' },
-  { value: '12,000+', label: 'Group Workforce' },
-  { value: '11+', label: 'Manufacturing Units' },
+  { value: '7,000+', label: 'Group Workforce' },
+  { value: '6', label: 'Manufacturing Units' },
 
 ];
